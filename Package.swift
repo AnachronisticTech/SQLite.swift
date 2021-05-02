7a1494a701863248079a69d02cf5b2f363926704
Daniel Marriner <danielmarriner@anachronistic-tech.co.uk>
2021-05-02 14:53:55 +0300

    Re-implemented Apple support

--- Package.swift
+++ Package.swift
@@ -3,25 +3,45 @@ import PackageDescription
 
 let package = Package(
     name: "SQLite.swift",
-    products: [.library(name: "SQLite", targets: ["SQLite"])],
-    targets: [
-        .target(name: "SQLite", dependencies: ["Csqlite3"]),
-        .testTarget(name: "SQLiteTests", dependencies: ["SQLite"], path: "Tests/SQLiteTests")
+    products: [
+        .library(name: "SQLite", targets: ["SQLite"])
     ],
     swiftLanguageVersions: [.v4, .v5]
 )
 
 #if os(Linux)
-    package.dependencies = [.package(url: "https://github.com/stephencelis/CSQLite.git", from: "0.0.3")]
+    package.dependencies = [
+        .package(
+            url: "https://github.com/stephencelis/CSQLite.git",
+            from: "0.0.3"
+        )
+    ]
     package.targets = [
-        .target(name: "SQLite", exclude: ["Extensions/FTS4.swift", "Extensions/FTS5.swift"]),
-        .testTarget(name: "SQLiteTests", dependencies: ["SQLite"], path: "Tests/SQLiteTests", exclude: [
-            "FTS4Tests.swift",
-            "FTS5Tests.swift"
-        ])
+        .target(
+            name: "SQLite",
+            exclude: [
+                "Extensions/FTS4.swift",
+                "Extensions/FTS5.swift"
+            ]
+        ),
+        .testTarget(
+            name: "SQLiteTests",
+            dependencies: ["SQLite"],
+            path: "Tests/SQLiteTests",
+            exclude: [
+                "FTS4Tests.swift",
+                "FTS5Tests.swift"
+            ]
+        )
     ]
 #elseif os(Windows)
-    package.dependencies = [.package(name: "CSQLite", url: "https://github.com/AnachronisticTech/CSQLite", from: "3.35.5")]
+    package.dependencies = [
+        .package(
+            name: "CSQLite",
+            url: "https://github.com/AnachronisticTech/CSQLite",
+            from: "3.35.5"
+        )
+    ]
     package.targets = [
         .target(
             name: "SQLite", 
@@ -39,6 +59,27 @@ let package = Package(
             exclude: [
                 "FTS4Tests.swift",
                 "FTS5Tests.swift",
+                "Info.plist",
+                "fixtures/encrypted-3.x.sqlite",
+                "fixtures/encrypted-4.x.sqlite"
+            ]
+        )
+    ]
+#else
+    package.targets = [
+        .target(
+            name: "SQLite",
+            dependencies: ["SQLiteObjc"],
+            exclude: [
+                "Info.plist"
+            ]
+        ),
+        .target(name: "SQLiteObjc"),
+        .testTarget(
+            name: "SQLiteTests",
+            dependencies: ["SQLite"],
+            path: "Tests/SQLiteTests",
+            exclude: [
                 "Info.plist",
                 "fixtures/encrypted-3.x.sqlite",
                 "fixtures/encrypted-4.x.sqlite"
